e7402cad494dc49bf1f712cee1f8c05917d60df0
Alex B <abailon949@gmail.com>
2025-02-19 21:03:59 -0600

    Added icons

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeStack from './HomeStack';
 import SearchScreen from '../screens/SearchScreen';
 import NotificationsScreen from '../screens/NotificationsScreen';
@@ -12,22 +13,26 @@ const screens = [
   {
     name: 'Home',
     component: HomeStack,
-    title: 'Home'
+    title: 'Home',
+    icon: 'home',
   },
   {
     name: 'Search',
     component: SearchScreen,
-    title: 'Search'
+    title: 'Search',
+    icon: 'magnify',
   },
   {
     name: 'Notifications',
     component: NotificationsScreen,
-    title: 'Notifications'
+    title: 'Notifications',
+    icon: 'bell',
   },
   {
     name: 'Settings',
     component: SettingsScreen,
-    title: 'Settings'
+    title: 'Settings',
+    icon: 'cog',
   }
 ];
 
@@ -35,13 +40,29 @@ const BottomTabs = () => {
   const { setHeaderTitle } = useNavigationContext();
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#e41e31',
+        tabBarInactiveTintColor: '#999',
+        tabBarStyle: {
+          paddingBottom: 8,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      }}
+    >
       {screens.map(screen => (
         <Tab.Screen 
           key={screen.name}
           name={screen.name} 
           component={screen.component}
-          options={{ headerShown: false }}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name={screen.icon} color={color} size={24} />
+            ),
+          }}
           listeners={{
             focus: () => setHeaderTitle(screen.title)
           }}
